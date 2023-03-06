22fd618ffa96817b07325e08f8d13ed22fd46eef
Clément Broutin <clement.zogloglo@gmail.com>
2023-03-06 15:19:35 +0100

    HDR avec et sand demosaiquage

--- eclairage_soulage.m
+++ eclairage_soulage.m
@@ -21,23 +21,26 @@ diametre = size(masque,1);
 
 
 Image_eclairage = zeros(diametre, diametre, nb_im/7);
-Images = zeros(nb_raw/7, 7, 500, 500);
+Images_mosaic = zeros(nb_raw/7, 7, 500, 500);
+Images_gray = zeros(nb_raw/7, 7, 500, 500);
 ExposureTime = zeros(nb_raw/7, 7);
 raw_count = 1;
 im_count = 1;
 groupe_count = 1;
 
 for i = 1:size(list_dir, 1)
-    name = strcat(path, list_dir(i).name)
+    name = strcat(path, list_dir(i).name);
     if list_dir(i).name(end) == 'F'
 	raw = rawread(name);
 	info = rawinfo(name);
 	place = mod(raw_count, 7)+1;
-	Images(groupe_count, place, :, :) = raw(2500:2999, 5000:5499);
+	Images_mosaic(groupe_count, place, :, :) = raw(2500:2999, 5000:5499);
+	image_grise = rgb2gray(demosaic(raw, info.CFALayout));
+	Images_gray(groupe_count, place, :, :) = image_grise(2500:2999, 5000:5499);
 	ExposureTime(groupe_count, place) = info.ExifTags.ExposureTime;
-        raw_count = raw_count + 1;
-	if place == 2
-	    groupe_count = groupe_count + 1;
+        raw_count = raw_count + 1
+	if mod(raw_count, 7) == 1
+	    groupe_count = groupe_count + 1
 	end
     end
     if list_dir(i).name(end) == 'G'
@@ -56,17 +59,17 @@ for i = 1:size(list_dir, 1)
 	    end
 	    %imshow(image(list_x, list_y));hold on;
 	    %Eclairage = etalonnage(image(list_x, list_y), masque, [diametre/2 diametre/2], diametre/2);
-	    Image_eclairage(:, :, groupe_count) = image(list_x, list_y);
 	    %pause(1);
+	    Image_eclairage(:, :, groupe_count) = image(list_x, list_y);
 	end
         im_count = im_count + 1;
     end
 end
 
 Eclairage = etalonnage(Image_eclairage, masque, [diametre/2 diametre/2], diametre/2);
-zero = zeros(1, size(Eclairage, 2));
-figure,
-quiver3(zero, zero, zero, Eclairage(1, :), Eclairage(2, :), Eclairage(3, :));
+%zero = zeros(1, size(Eclairage, 2));
+%figure,
+%quiver3(zero, zero, zero, Eclairage(1, :), Eclairage(2, :), Eclairage(3, :));
 
 
-save SOULAGES Images ExposureTime Eclairage;
+save SOULAGES Images_mosaic Images_gray ExposureTime Eclairage ;
--- main_hdr.m
+++ main_hdr.m
@@ -6,23 +6,30 @@ H = taille_ecran(4);
 
 load SOULAGES;
 
-[nb_groupe, ~, row, col] = size(Images);
 
-image_hdr = zeros(nb_groupe, row, col);
-temps_expo = zeros(nb_groupe, row, col);
+[nb_groupe, ~, row, col] = size(Images_mosaic);
+
+image_hdr_mosaic = zeros(nb_groupe, row, col);
+image_hdr_gray = zeros(nb_groupe, row, col);
+temps_expo_mosaic = zeros(nb_groupe, row, col);
+temps_expo_gray = zeros(nb_groupe, row, col);
 masque = ones(row, col);
 exterieur = find(masque==0);
 
 for i = 1:nb_groupe
-    [image_hdr(i, :, :), temps_expo(i, :, :)] = hdr(squeeze(Images(i, :, :, :)), ExposureTime(i, :));
+    [image_hdr_mosaic(i, :, :), temps_expo_mosaic(i, :, :)] = hdr(squeeze(Images_mosaic(i, :, :, :)), ExposureTime(i, :));
+    [image_hdr_gray(i, :, :), temps_expo_gray(i, :, :)] = hdr(squeeze(Images_gray(i, :, :, :)), ExposureTime(i, :));
 end
-[rho, N] = estimation_HDR(image_hdr(:, :), temps_expo(:, :), Eclairage', masque(:));
-rho = reshape(rho, [row col]);
+[rho_mosaic, N_mosaic] = estimation_HDR(image_hdr_mosaic(:, :), temps_expo_mosaic(:, :), Eclairage', masque(:));
+[rho_gray, N_gray] = estimation_HDR(image_hdr_gray(:, :), temps_expo_gray(:, :), Eclairage', masque(:));
+
+%% Image mosaiquée
+rho_mosaic = reshape(rho_mosaic, [row col]);
 % Intégration du champ de normales :
-N(3,exterieur) = 1;			% Pour éviter les divisions par 0
-p_estime = reshape(-N(1,:)./N(3,:),size(masque));
+N_mosaic(3,exterieur) = 1;			% Pour éviter les divisions par 0
+p_estime = reshape(-N_mosaic(1,:)./N_mosaic(3,:),size(masque));
 p_estime(exterieur) = 0;
-q_estime = reshape(-N(2,:)./N(3,:),size(masque));
+q_estime = reshape(-N_mosaic(2,:)./N_mosaic(3,:),size(masque));
 q_estime(exterieur) = 0;
 z_estime_gray = integration_SCS(q_estime,p_estime);
 
@@ -34,4 +41,24 @@ z_estime_gray(exterieur) = NaN;
 
 % Affichage de l'albédo et du relief :
 figure('Name','Albedo et relief','Position',[0.6*L,0,0.2*L,0.7*H]);
-affichage_albedo_relief_tp(uint16(rho),z_estime_gray);
+affichage_albedo_relief_tp(uint16(rho_mosaic),z_estime_gray);
+
+%% Image grise
+rho_gray = reshape(rho_gray, [row col]);
+% Intégration du champ de normales :
+N_gray(3,exterieur) = 1;			% Pour éviter les divisions par 0
+p_estime = reshape(-N_gray(1,:)./N_gray(3,:),size(masque));
+p_estime(exterieur) = 0;
+q_estime = reshape(-N_gray(2,:)./N_gray(3,:),size(masque));
+q_estime(exterieur) = 0;
+z_estime_gray = integration_SCS(q_estime,p_estime);
+
+% Ambiguïté concave/convexe :
+if (z_estime_gray(floor(row/2),floor(col/2))<z_estime_gray(1,1))
+	z_estime_gray = z_estime_gray;
+end
+z_estime_gray(exterieur) = NaN;
+
+% Affichage de l'albédo et du relief :
+figure('Name','Albedo et relief','Position',[0.6*L,0,0.2*L,0.7*H]);
+affichage_albedo_relief_tp(uint16(rho_gray),z_estime_gray);
